7155aedca876f521215d851828662a2ec71afb9b
anand.sitaram <anandbs291@gmail.com>
2024-08-18 21:36:31 +0530

    first commit

---
+++ tests/newWindow.spec.js
@@ -0,0 +1,24 @@
+import {test,expect} from '@playwright/test'
+
+test('Test for New Window', async({browser})=>{
+
+    const context=await browser.newContext();
+    const page=await context.newPage();
+    await page.goto("https://practice.expandtesting.com/windows")
+     const newWindow= page.locator("a[href='/windows/new']")
+    //const newPage= await context.waitForEvent('page') //Listen for any new page - promise - pending, rejected,fullfilled
+
+    //await newWindow.click();//click on new link which opens new page
+
+    const [newPage] = await Promise.all(
+        [
+            context.waitForEvent('page'),//Listen for any new page - promise - pending, rejected,fullfilled
+            newWindow.click()//click on new link which opens new page
+        ]
+    )
+    
+    const newPageTxt=newPage.locator("div[class='example'] h1")
+    console.log(await newPageTxt.textContent())
+
+
+})
